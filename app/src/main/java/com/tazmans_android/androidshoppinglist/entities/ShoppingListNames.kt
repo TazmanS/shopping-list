fa47a342692b84a455adb54dc1aea17fd3a9214c
TazmanS <TazmanS123@gmail.com>
2023-03-03 14:39:52 +0200

    shopping list entity

---
+++ app/src/main/java/com/tazmans_android/androidshoppinglist/entities/ShoppingListNames.kt
@@ -0,0 +1,28 @@
+package com.tazmans_android.androidshoppinglist.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity("shopping_list_names")
+data class ShoppingListNames(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int?,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "time")
+    val time: String,
+
+    @ColumnInfo(name = "allItemCounter")
+    val allItemCounted: Int,
+
+    @ColumnInfo(name = "checkedItemCounter")
+    val checkedItemCounter: Int,
+
+    @ColumnInfo(name = "itemsIds")
+    val itemsIds: String,
+
+    ) : Serializable
