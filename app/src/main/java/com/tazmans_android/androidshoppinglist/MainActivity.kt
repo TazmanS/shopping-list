7c878a2e110b27691ce62bff30a043f3a2b84c7b
TazmanS <TazmanS123@gmail.com>
2023-03-03 14:19:58 +0200

    init

---
+++ app/src/main/java/com/tazmans_android/androidshoppinglist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tazmans_android.androidshoppinglist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
