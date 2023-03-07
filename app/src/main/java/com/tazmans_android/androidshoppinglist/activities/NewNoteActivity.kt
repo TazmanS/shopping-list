d4d7571e0405419a72a8c768b5f1297b08a7de4a
TazmanS <TazmanS123@gmail.com>
2023-03-07 13:38:44 +0200

    activity result launcher

--- app/src/main/java/com/tazmans_android/androidshoppinglist/activities/NewNoteActivity.kt
+++ app/src/main/java/com/tazmans_android/androidshoppinglist/activities/NewNoteActivity.kt
@@ -1,11 +1,14 @@
 package com.tazmans_android.androidshoppinglist.activities
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.tazmans_android.androidshoppinglist.R
 import com.tazmans_android.androidshoppinglist.databinding.ActivityNewNoteBinding
+import com.tazmans_android.androidshoppinglist.fragments.NoteFragment
 
 class NewNoteActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewNoteBinding
@@ -23,11 +26,20 @@ class NewNoteActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.id_save) {
-            finish()
+            setMyResult()
         } else if (item.itemId == android.R.id.home) {
             finish()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMyResult() {
+        val i = Intent().apply {
+            putExtra(NoteFragment.TITLE_KEY, binding.edTitle.text.toString())
+            putExtra(NoteFragment.DESC_KEY, binding.edDescription.text.toString())
+        }
+        setResult(Activity.RESULT_OK, i)
+        finish()
     }
 
     private fun actionBarSettings() {
--- app/src/main/java/com/tazmans_android/androidshoppinglist/fragments/NoteFragment.kt
+++ app/src/main/java/com/tazmans_android/androidshoppinglist/fragments/NoteFragment.kt
@@ -1,10 +1,14 @@
 package com.tazmans_android.androidshoppinglist.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.activityViewModels
 import com.tazmans_android.androidshoppinglist.activities.MainApp
 import com.tazmans_android.androidshoppinglist.activities.NewNoteActivity
@@ -13,17 +17,19 @@ import com.tazmans_android.androidshoppinglist.db.MainViewModel
 
 class NoteFragment : BaseFragment() {
     private lateinit var binding: FragmentNoteBinding
+    private lateinit var editLauncher: ActivityResultLauncher<Intent>
 
     private val mainViewModel: MainViewModel by activityViewModels {
         MainViewModel.MainViewModelFactory((context?.applicationContext as MainApp).database)
     }
 
     override fun onClickNew() {
-        startActivity(Intent(activity, NewNoteActivity::class.java))
+        editLauncher.launch(Intent(activity, NewNoteActivity::class.java))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        onEditResult()
     }
 
     override fun onCreateView(
@@ -34,7 +40,19 @@ class NoteFragment : BaseFragment() {
         return binding.root
     }
 
+    private fun onEditResult() {
+        editLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == Activity.RESULT_OK) {
+                Log.d("MyLog", "title: ${it.data?.getStringExtra(TITLE_KEY)}")
+                Log.d("MyLog", "description: ${it.data?.getStringExtra(DESC_KEY)}")
+            }
+        }
+    }
+
     companion object {
+        const val TITLE_KEY = "title_key"
+        const val DESC_KEY = "desc_key"
+
         @JvmStatic
         fun newInstance() = NoteFragment()
     }
